e1a0b3bdfa392bc1c189215d247e272df6c2e875
Francesco <francescopeverell2@gmail.com>
2022-02-02 18:38:27 +0100

    working on plots

---
+++ generate_plots.py
@@ -0,0 +1,22 @@
+# This script is used to generate plots from the different benchmark runs
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import argparse
+import re
+
+parser = argparse.ArgumentParser(
+    description="Plot different recorded metrics for a set of benchmark runs")
+
+parser.add_argument('--x', dest='x_list', type=str, required=True,
+help='[REQUIRED]\tCommand used to speciy x-axes as BENCH_NAME:TAG_REGEX')
+parser.add_argument('--y', dest='y_metric', type=str, required=True,
+help='[REQUIRED]\tCommand used to speciy which metric to plot on the y axis')
+parser.add_argument('--name', dest='graph_name', type=str, required=True,
+help='[REQUIRED]\tCommand used to speciy the final graph name')
+parser.add_argument('--format', dest='format', type=str, default='svg',
+help='[OPTIONAL]\tAllows to specify the plot format (e.g. svg,pdf)')
+
+args = parser.parse_args()
