5b851561cb474d281b4e8f6983058aa4d5a4c693
francesco-peverelli <francesco.peverelli@polimi.it>
2022-02-07 13:39:19 +0000

    B.2 results

--- run_hpcg_cpu_benchmarks.sh
+++ run_hpcg_cpu_benchmarks.sh
@@ -1,11 +1,11 @@
 
 for ppn in {1,2}; do
-	for n in {4,8,16,32,64,128}; do
+	for n in {104,52,26}; do
 		for i in {1..1}; do
 			np=$((${n}/${ppn}))
-			ont=$((128/${n}))
+			ont=$((104/${n}))
 			bcmd="mpiexec.hydra -n ${np} -ppn ${ppn} env OMP_NUM_THREADS=${ont} KMP_AFFINITY=granularity=fine,compact,1,0 /opt/intel/oneapi/mkl/2022.0.2/benchmarks/hpcg/bin/xhpcg_avx2"
-			cmd="python3 run_wrapper.py --cmd \"$bcmd\" --bench hpcg_cpu --tag $1x_${np}n_${ppn}ppn_${ont}ont_p220 --dir \"../\" --cpu-power 1 --cpu-threshold 220"
+			cmd="python3 run_wrapper.py --cmd \"$bcmd\" --bench hpcg_cpu --tag $1x_${np}n_${ppn}ppn_${ont}ont_p220_bm2 --dir \"../\" --cpu-power 1 --cpu-threshold 90"
 			echo "running ${cmd}"
 			eval $cmd
 			echo "Done!"
