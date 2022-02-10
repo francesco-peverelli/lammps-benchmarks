53fd924da3ebdc6b25a2fce89612c8ae1ba4dde3
francesco-peverelli <francesco.peverell@polimi.it>
2022-02-10 11:19:30 +0000

    Saving state

--- run_lammps_intel.sh
+++ run_lammps_intel.sh
@@ -2,7 +2,7 @@ for n in {1,2,4,8,16,32,64,128}; do
 	for iter in {1..15}; do
 		ont=$((128/${n}))
 		bcmd="mpiexec -np ${n} -env OMP_NUM_THREADS=${ont} ../src/lmp_intel_cpu_intelmpi -in $1 -pk intel 0 omp ${ont} mode mixed -sf intel"
-		cmd="python3 run_wrapper.py --cmd \"$bcmd\" --bench lammps --tag $1_${n}n_${ont}ont_32k_220p --dir \"../../../lammps/bench\" --cpu-power 1 --cpu-threshold 220"
+		cmd="python3 run_wrapper.py --cmd \"$bcmd\" --bench lammps --tag $1_${n}n_${ont}ont_${2}k_220p --dir \"../../../lammps/bench\" --cpu-power 1 --cpu-threshold 220"
 		echo "running ${cmd}"
 		eval ${cmd}
 		echo "Done!"
