d2a6f6f6bf60064a276191aff5656c6b5a50a7ea
francesco-peverelli <francesco.peverell@polimi.it>
2022-02-15 17:14:43 +0000

    Added strong scaling experiments

--- run_hpcg_cpu_benchmarks.sh
+++ run_hpcg_cpu_benchmarks.sh
@@ -1,14 +1,15 @@
 
-for ppn in {1,2}; do
-	for n in {2,4,8,16,32,64,128}; do
-		for i in {1..1}; do
-			np=$((${n}/${ppn}))
-			ont=$((128/${n}))
-			bcmd="mpiexec.hydra -n ${np} -ppn ${ppn} env OMP_NUM_THREADS=${ont} KMP_AFFINITY=granularity=fine,compact,1,0 /opt/intel/oneapi/mkl/2022.0.2/benchmarks/hpcg/bin/xhpcg_avx2"
-			cmd="python3 run_wrapper.py --cmd \"$bcmd\" --bench hpcg_cpu --tag $1x_${np}n_${ppn}ppn_${ont}ont_p220 --dir \"../\" --cpu-power 1 --cpu-threshold 220"
+for ont in {1,2}; do
+	#for n in {2,4,8,16,32,64,128}; do
+		for i in {1..10}; do
+			#np=$((${n}/${ppn}))
+			#ont=$((128/${n}))
+			n=64
+			bcmd="mpiexec.hydra -n ${n} env OMP_NUM_THREADS=${ont} KMP_AFFINITY=granularity=fine,compact,1,0 /opt/intel/oneapi/mkl/2022.0.2/benchmarks/hpcg/bin/xhpcg_avx2"
+			cmd="python3 run_wrapper.py --cmd \"$bcmd\" --bench hpcg_cpu --tag $1x_${n}n_1ppn_${ont}ont_p210 --dir \"../\" --cpu-power 1 --cpu-threshold 210"
 			echo "running ${cmd}"
 			eval $cmd
 			echo "Done!"
 		done
-	done
+	#done
 done
