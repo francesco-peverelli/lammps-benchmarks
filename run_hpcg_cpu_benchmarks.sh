39bffb89936bf090350af4e3e316746fc870a81c
francesco-peverelli <francesco.peverell@polimi.it>
2022-03-14 09:08:07 +0000

    Added 64 ont

--- run_hpcg_cpu_benchmarks.sh
+++ run_hpcg_cpu_benchmarks.sh
@@ -1,9 +1,10 @@
 
-for ont in {1,2,4,8,16,32}; do
+#for ont in {1,2,4,8,16,32}; do
 	#for n in {64}; do
 		for i in {1..5}; do
 			#np=$((${n}/${ppn}))
 			#ont=$((128/${n}))
+			ont=64
 			n=2
 			bcmd="mpiexec.hydra -n ${n} env OMP_NUM_THREADS=${ont} KMP_AFFINITY=granularity=fine,compact,1,0 /opt/intel/oneapi/mkl/2022.0.2/benchmarks/hpcg/bin/xhpcg_avx2"
 			cmd="python3 run_wrapper.py --cmd \"$bcmd\" --bench hpcg_cpu --tag $1x_${n}n_dsocket_${ont}ont_p190 --dir \"../\" --cpu-power 1 --cpu-threshold 190"
@@ -12,4 +13,4 @@ for ont in {1,2,4,8,16,32}; do
 			echo "Done!"
 		done
 	#done
-done
+#done
