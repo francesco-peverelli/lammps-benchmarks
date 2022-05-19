64e74c75dd2b874231d520873634a2b41584f8ee
Francesco Peverelli <francesco.peverelli@polimi.it>
2022-05-19 12:26:22 +0000

    Modified prof script

--- profile_lammps_intel.sh
+++ profile_lammps_intel.sh
@@ -4,7 +4,7 @@ echo $pwd
 for n in {1,2,4,8,16,32,64}; do
 #        for iter in {1..5}; do
 		ont=1
-		bcmd="mpiexec -mps -np ${n} env OMP_NUM_THREADS=${ont} KMP_AFFINITY=granularity=fine,compact,1,0 ../src/lmp_intel_cpu_intelmpi -in $1 -pk intel 0 omp ${ont} mode mixed -sf intel"
+		bcmd="mpiexec -aps -mps -np ${n} env OMP_NUM_THREADS=${ont} KMP_AFFINITY=granularity=fine,compact,1,0 ../src/lmp_intel_cpu_intelmpi -in $1 -pk intel 0 omp ${ont} mode mixed -sf intel"
 		cmd="$bcmd &> $start_dir/lammps/prof/$1_${n}n_${ont}ont_${2}k_profiling.txt"
 		echo "running ${cmd}"
 		eval ${cmd}
