a5d9abcdd25797efb2b4d5cc823013068c14c391
Francesco Peverelli <francesco.peverelli@polimi.it>
2022-05-19 15:02:29 +0000

    Added CPU breakdown

--- profile_lammps_intel.sh
+++ profile_lammps_intel.sh
@@ -5,7 +5,7 @@ for n in {1,2,4,8,16,32,64}; do
 #        for iter in {1..5}; do
 		ont=1
 		bcmd="mpiexec -aps -mps -np ${n} env OMP_NUM_THREADS=${ont} KMP_AFFINITY=granularity=fine,compact,1,0 ../src/lmp_intel_cpu_intelmpi -in $1 -pk intel 0 omp ${ont} mode mixed -sf intel"
-		cmd="$bcmd &> $start_dir/lammps/prof/$1_${n}n_${ont}ont_${2}k_profiling.txt"
+		cmd="$bcmd > $start_dir/lammps/prof/$1_${n}n_${ont}ont_${2}k_profiling.txt"
 		echo "running ${cmd}"
 		eval ${cmd}
 		eval "rm log.*"
