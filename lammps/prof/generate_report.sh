5fe529b5acfcb5f46cc964e9092c86cccb29b284
Francesco Peverelli <francesco.peverelli@polimi.it>
2022-05-23 13:47:12 +0000

    Added csv

--- lammps/prof/generate_report.sh
+++ lammps/prof/generate_report.sh
@@ -2,14 +2,15 @@ array=( $(find -type d -regex ".*_aps") )
 
 for dir in ${array[@]}
 do
+	cmd_csv="aps-report --format csv"
 	cmd="aps --report $dir"
-	echo $cmd
-	$cmd
-	for i in *.html; do
-		mv $i ${dir}_report
-	done
+	echo $cmd_csv
+	${cmd_csv} ${dir} > ${dir}.csv
+	#for i in *.html; do
+	#	mv $i ${dir}_report
+	#done
 done
 
-for i in *_report; do
-	mv $i ${i}.html
-done
+#for i in *_report; do
+#	mv $i ${i}.html
+#done
