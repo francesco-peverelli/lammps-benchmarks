3e4cf72e9a2c3f52e935493a1333bb8dcdb41488
francesco.peverelli <francesco.peverelli@polimi.it>
2022-05-26 10:41:22 +0200

    Added graph generation in seaborn

---
+++ lammps/prof/aggregate_mpi_data.py
@@ -0,0 +1,85 @@
+import pandas as pd 
+import os 
+
+experiments = ["chain", "chute", "eam", "lj", "rhodo"]
+mpi_nproc = [1, 2, 4, 8, 16, 32, 64]
+nk_atoms = [32, 256, 864, 2048]
+
+def get_file_name(bench, nproc, nk_atoms):
+    name = "in." + bench 
+    if nk_atoms > 32 and ((bench == "chain") or (bench == "rhodo")):
+        name += ".scaled.timer"
+    else:
+        name += ".timer"
+    
+    name += "_" + str(nproc) + "n_1ont_" + str(nk_atoms) + "k_aps.csv"
+    return name
+
+mpi_funcs = set() # MPI functions
+mpi_data = []
+
+files = os.listdir(os.getcwd())
+
+for fname in files:
+    if fname.endswith(".csv") and (fname != "aggregate_mpi_stats.csv"):
+        try:
+            file = open(fname, "r")
+            in_mpi_calls = False
+            in_total_time = False
+            params = fname.split('_')
+            print(params)
+            bench = params[0].replace(".scaled","")
+            times_dict = {"Benchmark" : bench} # mapping func : time%
+            times_dict['Size'] = int(params[3][:-1])
+            times_dict['Processes'] = int(params[1][:-1])
+
+            for line in file:
+                if line.find("Function summary for all Ranks") >= 0:
+                    in_mpi_calls = True
+                if line[0:7] == "| TOTAL":
+                    in_mpi_calls = False
+                if in_total_time  and (line[0:7] == "| TOTAL"):
+                    in_total_time = False
+                    tokens = line.split(";")
+                    tokens = [x.strip() for x in tokens]
+                    times_dict['MPI_Time'] = float(tokens[2])
+                    times_dict['MPI_(%)'] = float(tokens[3])
+                    if 'MPI_Time' not in mpi_funcs:
+                        mpi_funcs.add('MPI_Time')
+                    if 'MPI_(%)' not in mpi_funcs:
+                        mpi_funcs.add('MPI_(%)')
+                if line.find("| MPI Time per Rank for all Ranks") >= 0:
+                    in_total_time = True
+                if in_mpi_calls:
+                    if line.find("MPI_") >= 0:
+                        tokens = line.split(";")
+                        tokens = [x.strip() for x in tokens]
+                        if tokens[0] not in mpi_funcs:
+                            mpi_funcs.add(tokens[0])
+                        times_dict[tokens[0]] = float(tokens[2])
+            mpi_data.append(times_dict)
+        except IOError:
+            print("File not found: " + fname)
+
+for el in mpi_data:
+    for func in mpi_funcs:
+        if func not in el.keys():
+            el[func] = 0.0
+
+header = ["Benchmark"] + list(mpi_funcs)
+df = pd.DataFrame(columns=header)
+
+for row in mpi_data:
+    df = df.append(row, ignore_index=True)
+col = df['MPI_Time']
+df.drop(labels=['MPI_Time'], axis=1,inplace = True)
+df.insert(1, 'MPI_Time', col)
+col = df['MPI_(%)']
+df.drop(labels=['MPI_(%)'], axis=1,inplace = True)
+df.insert(1, 'MPI_(%)', col)
+df = df.sort_values(['Benchmark','Size','Processes'])
+df.groupby(['Size','Processes'])
+print(df)
+df.to_csv("aggregate_mpi_stats.csv", index=False)
+df.to_excel("aggregate_mpi_stats.xlsx", index=False)
+            
---
+++ lammps/prof/charts4paper.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns              
+import matplotlib.ticker as tkr
+import matplotlib.gridspec as gridspec
+from matplotlib.patches import Patch
+import os
+import matplotlib.lines as lines
+import matplotlib.ticker as ticker
+from itertools import islice
+#from plot_utils import *
+
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(islice(iterable, n))
+#PALETTE = [COLORS["peach2"], COLORS["g1"]]
+#PALETTE_B = [COLORS["b3"], COLORS["b3"]]
+#PALETTE_GW = [COLORS[r] for r in ["gw3","gw2","gw1"]]
+#HATCHES = ['', '/'*4, '\\'*4]
+
+mpi_funcs = ["MPI_Scan", "MPI_Comm_dup", "MPI_Comm_size", "MPI_Alltoallv", "MPI_Cart_shift", \
+    "MPI_Finalize", "MPI_Wait", "MPI_Reduce", "MPI_Comm_rank", "MPI_Allgather", "MPI_Reduce_scatter", \
+    "MPI_Barrier", "MPI_Sendrecv", "MPI_Waitany", "MPI_Cart_rank", "MPI_Cart_create", "MPI_Irecv", \
+    "MPI_Cart_get", "MPI_Allreduce", "MPI_Comm_free", "MPI_Bcast", "MPI_Init", "MPI_Alltoall", "MPI_Send"]
+
+data = pd.read_csv("aggregate_mpi_stats.csv", sep=',')
+
+data['Benchmark'] = data['Benchmark'].apply(lambda x: x[3:-6])
+
+mpi_tot_data = data.melt(id_vars=["Processes", "Size", "Benchmark"], value_vars=["MPI_(%)"])
+
+sns.set_style("whitegrid")
+g = sns.catplot(data=mpi_tot_data, col='Processes', hue='Benchmark', x='Size', y='value', \
+    kind='point', palette='mako')
+g.set_axis_labels("Problem Size [K atoms]","MPI Total Time [%]")
+g.savefig("mpi_tot_data.png")
+
+top_N = 5
+vals = {}
+for f in mpi_funcs:
+    vals[f] = data[f].mean()
+
+vals = {k: v for k, v in sorted(vals.items(), reverse=True, key=lambda item: item[1])}
+
+top_vals = take(top_N, vals.items())
+top_funcs = [top_val[0] for top_val in top_vals]
+
+mpi_func_data = data.melt(id_vars=["Processes", "Size", "Benchmark"], value_vars=top_funcs)
+g2 = sns.catplot(data=mpi_func_data, col='Processes', hue='variable', row='Benchmark', x='Size', y='value', \
+    kind='bar', palette='CMRmap')
+g2.set_axis_labels("Problem Size [K atoms]","MPI Function Time [%]")
+g2.savefig("mpi_funcs.png")
