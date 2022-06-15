fd52b17c2adac383462236628ced4220e7a8fac9
Francesco Peverelli <francesco.peverelli@polimi.it>
2022-06-15 12:14:44 +0000

    Setting up batch running

--- run_lammps_gpu.py
+++ run_lammps_gpu.py
@@ -1,11 +1,12 @@
-import os
-import sys
+
 
 """ Argv 1: benchmark input file name
     Argv 2: problem size string (e.g. 32k, 2M, ...)
     Argv 3: problem size index [0-3]
 """
 
+import os
+import sys
 
 gpu_mpi_dict = [{1 : 6, 2 : 12, 4 : 12, 6 : 18, 8 : 24},
         {1 : 16, 2 : 28, 4 : 32, 6 : 48, 8 : 48},
@@ -22,6 +23,8 @@ for gpu,mpi in gpu_mpi_dict[problem_idx].items():
     for k in range(0,10):
         bench_cmd="'mpiexec -np " + str(mpi) + " env OMP_NUM_THREADS=1 ../bin/lmp_intel_cpu_intelmpi -in " + bench_in + " -sf gpu -pk gpu " + str(gpu) + "'"
         tag = bench_in + "_" + str(gpu) + "g_" + str(mpi) + "n_"+ sys.argv[2]
-        cmd="python3 run_wrapper.py --cmd " + bench_cmd + " --bench lammps_gpu --tag " + tag + " --dir " + bench_dir + " --arch BM.GPU3.8 --gpu-power " + str(gpu) + " --cpu-power 1 --gpu-threshold " + str(gthreshold)
-        print("running " + cmd)
+        cmd="python3 run_wrapper.py --cmd " + bench_cmd + " --bench lammps_gpu --tag " + tag + " --dir " + bench_dir + " --arch BM.GPU3.8 --gpu-power 0 --cpu-power 0"
+#        cmd="python3 run_wrapper.py --cmd " + bench_cmd + " --bench lammps_gpu --tag " + tag + " --dir " + bench_dir + " --arch BM.GPU3.8 --gpu-power " + str(gpu) + " --cpu-power 1 --gpu-threshold " + str(gthreshold)
+
+
         os.system(cmd) 
---
+++ run_lammps_gpu_batch.py
@@ -0,0 +1,14 @@
+import os
+
+benchmarks= ["rhodo", "lj", "eam", "chain"]
+sizes =  ["", ".scaled", ".scaled_864", ".scaled_2048"]
+katoms = ["32", "256", "864", "2048"]
+
+for b in benchmarks:
+    snum = 0
+    for s in sizes:
+        in_name = "in." + b + s 
+        cmd="python3 run_lammps_gpu.py " + in_name + " " + katoms[snum] + "k  " + str(snum)
+        print("Executing" + cmd)
+        os.system(cmd)
+        snum = snum + 1
