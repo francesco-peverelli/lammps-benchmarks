f25a0f6e8d7e2b00473ea5a4626cd4471741fdde
Francesco Peverelli <francesco.peverelli@polimi.it>
2022-06-23 15:13:13 +0000

    Fixing chute

--- run_lammps_intel_batch.py
+++ run_lammps_intel_batch.py
@@ -2,8 +2,11 @@ import subprocess
 import os
 
 benchmarks = ["chute"]
-variants = ["",".scaled",".scaled_864",".scaled_2048"]
-dims = [32, 256, 864, 2048]
+#variants = ["",".scaled",".scaled_864",".scaled_2048"]
+#dims = [32, 256, 864, 2048]
+variants = ["",]
+dims = [32]
+
 
 for b in benchmarks:
     i = 0
