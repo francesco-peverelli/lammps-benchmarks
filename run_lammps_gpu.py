8232893d497c8628c430ee6a7a4e1d52e1636173
Francesco Peverelli <francesco.peverelli@polimi.it>
2022-06-24 07:52:43 +0000

    Modifying runs

--- run_lammps_gpu.py
+++ run_lammps_gpu.py
@@ -23,8 +23,8 @@ for gpu,mpi in gpu_mpi_dict[problem_idx].items():
     for k in range(0,10):
         bench_cmd="'mpiexec -np " + str(mpi) + " env OMP_NUM_THREADS=1 ../bin/lmp_intel_cpu_intelmpi -in " + bench_in + " -sf gpu -pk gpu " + str(gpu) + "'"
         tag = bench_in + "_" + str(gpu) + "g_" + str(mpi) + "n_"+ sys.argv[2]
-        cmd="python3 run_wrapper.py --cmd " + bench_cmd + " --bench lammps_gpu --tag " + tag + " --dir " + bench_dir + " --arch BM.GPU3.8 --gpu-power 0 --cpu-power 0"
-#        cmd="python3 run_wrapper.py --cmd " + bench_cmd + " --bench lammps_gpu --tag " + tag + " --dir " + bench_dir + " --arch BM.GPU3.8 --gpu-power " + str(gpu) + " --cpu-power 1 --gpu-threshold " + str(gthreshold)
+#        cmd="python3 run_wrapper.py --cmd " + bench_cmd + " --bench lammps_gpu --tag " + tag + " --dir " + bench_dir + " --arch BM.GPU3.8 --gpu-power 0 --cpu-power 0"
+        cmd="python3 run_wrapper.py --cmd " + bench_cmd + " --bench lammps_gpu --tag " + tag + " --dir " + bench_dir + " --arch BM.GPU3.8 --gpu-power " + str(gpu) + " --cpu-power 1 --gpu-threshold " + str(gthreshold)
 
 
         os.system(cmd) 
